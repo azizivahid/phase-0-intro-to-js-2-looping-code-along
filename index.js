8ec938a01e32022dac99ceeea617a8b7ae1e5ddc
azizivahid <azizi.vahid@yahoo.com>
2023-03-10 17:39:40 -0500

    Adds

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+var otherArray = [];
+let i = 0
+
+function  writeCards (){
+
+ otherArray = ["Thank you, Guadalupe, for the wonderful surprise gift!",
+                "Thank you, Ollie, for the wonderful surprise gift!",
+                "Thank you, Aki, for the wonderful surprise gift!"]
+   
+   return otherArray 
+}
+
+function countDown(number) {
+    while (number >= 0){
+        console.log (number )
+        number = number -1
+}
+
+}
